06a83f703d47a9ad9280d419c8dcf449d1d8fb2e
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-11-26 03:29:53 +0700

    complete 2 need

--- src/Controllers/v1/stat.controllers.ts
+++ src/Controllers/v1/stat.controllers.ts
@@ -3,42 +3,39 @@ import type { Request, Response, NextFunction } from "express"
 
 export async function Get(req: Request, res: Response, next: NextFunction) {
 
-  let currentDate = new Date();
-  let sixmon = new Date();
-  sixmon.setMonth(sixmon.getMonth() - 6)
-  const statSixMount = await prisma.danger.count({
-    where: {
-      timelineStart: {
-        lte: currentDate,
-        gte: sixmon,
-      }
-    }
-  })
+    let currentDate = new Date();
+    let sixmon = new Date();
+    sixmon.setMonth(sixmon.getMonth() - 6)
+    const statSixMount = await prisma.danger.count({
+        where: {
+            timelineStart: {
+                lte: currentDate,
+                gte: sixmon,
+            }
+        }
+    })
 
-  const danger = await prisma.danger.findMany()
+    const danger = await prisma.danger.findMany()
 
-  type RatingType = {
-    "1": number
-    "2": number
-    "3": number
-    "4": number
-    "5": number
-    "6": number
-    "7": number
-    "8": number
-    "9": number
-  }
+    const rating = danger.reduce((result, next) => {
+        result.push(next.rating)
+        return result
+    }, [] as number[])
 
-  const ratingCount = danger.reduce((acc, dangerItem) => {
-    if (dangerItem.rating >= 1 && dangerItem.rating <= 9) {
-      acc[dangerItem.rating.toString()] += 1;
-    }
-    return acc;
-  }, {} as Record<string,number>);
+    const rateList = rating.sort((a, b) => a + b).reduce((result, next) => {
+        if(isNaN(result[next])){
+            result[next] = 0
+        }
+        
+        result[next] = Number(result[next]) + next
+        return result
+    }, {} as Record<number, number>)
 
-  res.json({
-    method: "GET",
-    message: "Counting ",
-    statSixMount
-  })
+    res.json({
+        method: "GET",
+        message: "Counting ",
+        statSixMount,
+        rateList
+
+    })
 }
