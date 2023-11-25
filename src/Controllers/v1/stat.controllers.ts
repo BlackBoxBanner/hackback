f146787d4eee2ca3252f3ffd71d7821829ab7eac
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-26 02:32:24 +0700

    feat: add ratingCount

--- src/Controllers/v1/stat.controllers.ts
+++ src/Controllers/v1/stat.controllers.ts
@@ -3,15 +3,38 @@ import type { Request, Response, NextFunction } from "express"
 
 export async function Get(req: Request, res: Response, next: NextFunction) {
 
-    let currentDate = new Date();
-    let sixmon = new Date();
-    sixmon.setMonth(sixmon.getMonth() - 6)
-    const statSixMount = await prisma.danger.count({where: {
-        timelineStart: {
-            lte: currentDate,
-            gte: sixmon,
-        }
-    }} )
+  let currentDate = new Date();
+  let sixmon = new Date();
+  sixmon.setMonth(sixmon.getMonth() - 6)
+  const statSixMount = await prisma.danger.count({
+    where: {
+      timelineStart: {
+        lte: currentDate,
+        gte: sixmon,
+      }
+    }
+  })
+
+  const danger = await prisma.danger.findMany()
+
+  type RatingType = {
+    "1": number
+    "2": number
+    "3": number
+    "4": number
+    "5": number
+    "6": number
+    "7": number
+    "8": number
+    "9": number
+  }
+
+  const ratingCount = danger.reduce((acc, dangerItem) => {
+    if (dangerItem.rating >= 1 && dangerItem.rating <= 9) {
+      acc[dangerItem.rating.toString()] += 1;
+    }
+    return acc;
+  }, {} as Record<string,number>);
 
   res.json({
     method: "GET",
