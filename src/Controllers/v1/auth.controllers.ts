becc79ad3f6388d57a5a46c21ce5d91c0995c384
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-25 23:01:39 +0700

    feat: add signout function

--- src/Controllers/v1/auth.controllers.ts
+++ src/Controllers/v1/auth.controllers.ts
@@ -1,5 +1,6 @@
 import prisma from "@/utils/prisma"
 import type { Request, Response, NextFunction } from "express"
+import { hash } from "bcrypt"
 
 export async function Get(req: Request, res: Response, next: NextFunction) {
   const data = await prisma.auth.findMany()
@@ -24,8 +25,7 @@ export async function signIn(req: Request, res: Response, next: NextFunction) {
     const result = await prisma.auth.findFirst({ where: { username, password } })
 
     if (result != null) {
-      res.cookie("auth-session", result)
-      return res.json({
+      return res.cookie("auth-session", await hash(JSON.stringify(result), 10)).json({
         method: "POST",
         message: "login succesfuly",
         result
@@ -82,6 +82,28 @@ export async function Post(req: Request, res: Response, next: NextFunction) {
       message: "Create user succesfuly",
       result
     })
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({
+        error: err.message
+      })
+    }
+    res.status(400).json({
+      error: "internal error"
+    })
+  }
+
+}
+
+export async function signOut(req: Request, res: Response, next: NextFunction) {
+
+  try {
+
+    return res.clearCookie("auth-session").json({
+      method: "POST",
+      message: "",
+    })
+
   } catch (err: unknown) {
     if (err instanceof Error) {
       res.status(400).json({
--- src/Routes/v1/auth.routers.ts
+++ src/Routes/v1/auth.routers.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { Get, Post, signIn } from "@/Controllers/v1/auth.controllers"
+import { Get, Post, signIn, signOut } from "@/Controllers/v1/auth.controllers"
 
 const routers = express.Router()
 
 routers.get("/user", Get)
 routers.post("/signin", signIn)
 routers.post("/signup", Post)
+routers.post("/signout", signOut)
 
 export default routers
